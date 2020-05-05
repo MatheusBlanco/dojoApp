550c64f1a6974bd99d686fcacc27bf54ac7d67f9
MatheusBlanco <msallesblanco@gmail.com>
2020-05-05 12:23:12 -0300

    failed attempt at adding animated cards

--- screens/PictureScreen.js
+++ screens/PictureScreen.js
@@ -4,35 +4,13 @@ import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react
 import { ScrollView } from 'react-native-gesture-handler';
 import { MonoText } from '../components/StyledText';
 import { Card, ListItem, Button, Icon } from 'react-native-elements';
+import SwipeCards from '../components/SwipeCard.js'
 
 export default function PictureScreen() {
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        
-        <View style={styles.photosContainer}>
-        <Card
-            title='Hai'
-            image={require('../assets/images/mom.png')}>
-                <Text style={{marginBottom:10}}>AlohaAlohaAlohaAlohaAlohaAlohaAlohaAlohaAloha</Text>
-        </Card>
-        <Card
-            title='Hai'
-            image={require('../assets/images/mom.png')}>
-                <Text style={{marginBottom:10}}>AlohaAlohaAlohaAlohaAlohaAlohaAlohaAlohaAloha</Text>
-        </Card>
-        <Card
-            title='Hai'
-            image={require('../assets/images/mom.png')}>
-                <Text style={{marginBottom:10}}>AlohaAlohaAlohaAlohaAlohaAlohaAlohaAlohaAloha</Text>
-        </Card>
-        </View>
-
-        <View style={styles.helpContainer}>
-         
-        </View>
-      </ScrollView>
-
+      
+      
     </View>
   );
 }
@@ -41,101 +19,13 @@ PictureScreen.navigationOptions = {
   header: null,
 };
 
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  photosContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  pictureScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
   navigationFilename: {
     marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  }
 });
