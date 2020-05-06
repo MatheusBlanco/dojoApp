f92f59aa7272812a678a5e8dacaed238d1374387
MatheusBlanco <msallesblanco@gmail.com>
2020-05-06 14:42:40 -0300

    Adding image card template

--- screens/PictureScreen.js
+++ screens/PictureScreen.js
@@ -1,31 +1,30 @@
-import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-import { MonoText } from '../components/StyledText';
-import { Card, ListItem, Button, Icon } from 'react-native-elements';
-import SwipeCards from '../components/SwipeCard.js'
 
 export default function PictureScreen() {
   return (
-    <View style={styles.container}>
+    <ScrollView>
+      <View style={{ marginTop: 25, flex: 1, alignItems: "center", justifyContent: "center",  }}>
+        <View style={{ backgroundColor: "#aeaeae", borderRadius: 10, overflow: "hidden" }}>
+          <View>
+            <Image
+              source={require("../assets/images/mom.png")}
+              style={{
+                height: 340,
+                width: 350
+              }}
+            />
+          </View>
+          <View style={{ padding: 10, width: 155 }}>
+            <Text>Title</Text>
+            <Text style={{ color: "#777", paddingTop: 5 }}>
+              Description of the image
+            </Text>
+          </View>
+        </View>
+      </View>
       
-      
-    </View>
+    </ScrollView>
   );
 }
-
-PictureScreen.navigationOptions = {
-  header: null,
-};
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  }
-});
